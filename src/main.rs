9d047aaa2965a03c574a9ed7211bb8af2650d771
Maurice <mauricegolverdingen@hotmail.com>
2022-03-24 16:15:11 +0100

    Moved some functionality to other branch

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ use std::fs::File;
 use std::io::{prelude::*};
 use std::os::unix::process::CommandExt;
 use std::process::{Command, Stdio};
-use std::time::Duration;
-use std::{fs, env, thread};
+use std::{fs, env};
 
 use chrono::Utc;
 
@@ -15,8 +14,6 @@ const ART: &str = "
 ██   ██               ██    ██ ██         ██       ██       ██    
 ██   ██                ██████  ███████    ██       ██       ██  
 ";
-
-const SERVICE: &str = "system-auth";
 
 fn read_input(tty: &mut File, mask: Option<char>) -> String {
     let mut chars = Vec::new();
@@ -63,7 +60,6 @@ fn main() {
         .expect("TTY not specified")
         .parse().expect("TTY is not a number, must be 1-12");
 
-    let use_custom_login = env::var("CUSTOMLOGIN").is_ok();
     let dont_clear_screen = env::var("NOCLEAR").is_ok();
 
     if nr_tty < 1 || nr_tty > 12 {
@@ -90,62 +86,5 @@ fn main() {
     tty.flush().unwrap();
     
     let username = read_input(&mut tty, None);
-
-    // If we want to use the password prompt from login:
-    if !use_custom_login {
-        login(tty, &username, true);
-    } else {
-        ask_and_validate_pass(&username, tty);
-    }
-}
-
-fn ask_and_validate_pass(username: &str, tty: File) {
-    let mut tty = tty;
-    write!(tty, "Password: ").unwrap();
-    tty.flush().unwrap();
-
-    let password = read_input(&mut tty, Some('*'));
-
-    let mut auth =
-        pam::Authenticator::with_password(SERVICE).expect("Failed to start the PAM service");
-
-    auth.get_handler().set_credentials(username, &password);
-
-    let authenticate = auth.authenticate();
-    let session = auth.open_session();
-
-    if authenticate.is_ok() && session.is_ok() {
-        write!(tty, "\x1b[1;32m").unwrap();
-        writeln!(tty, "Authentication SUCCEED :)").unwrap();
-        write!(tty, "\x1b[0m").unwrap(); //reset color
-
-        login(tty, username, false);
-    } else {
-        write!(tty, "\x1b[1;31m").unwrap();
-        writeln!(tty, "Authentication failed :(").unwrap();
-
-        if authenticate.is_err() {
-            writeln!(
-                tty,
-                "Failed to authenticate: {}",
-                authenticate.err().unwrap().to_string()
-            )
-            .unwrap();
-        }
-
-        if session.is_err() {
-            writeln!(
-                tty,
-                "Failed to open session: {}",
-                session.err().unwrap().to_string()
-            )
-            .unwrap();
-        }
-
-        write!(tty, "\x1b[0m").unwrap(); //reset color
-        tty.flush().unwrap();
-        
-        thread::sleep(Duration::from_secs(5));
-        std::process::exit(1);
-    }
+    login(tty, &username, true);
 }
